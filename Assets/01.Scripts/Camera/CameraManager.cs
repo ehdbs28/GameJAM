0a0571823632396207a6f445b5ad1e4453010866
ehdbs28 <doyun5373@naver.com>
2022-07-11 21:16:32 +0900

    카메라 쉐이크 기능 추가
    //테스트코드 삭제 해야함

--- Assets/01.Scripts/Camera/CameraManager.cs
+++ Assets/01.Scripts/Camera/CameraManager.cs
@@ -7,14 +7,18 @@ public class CameraManager : MonoBehaviour
 {
     public static CameraManager Instance = null;
 
-    [SerializeField] private CinemachineVirtualCamera _mainVcam;
-
     [SerializeField] private List<Transform> _stageTrm = new List<Transform>();
+
+    private CinemachineVirtualCamera _mainVcam;
+    private CinemachineBasicMultiChannelPerlin _mainPerlin;
 
     int i = 1;
 
     private void Start()
     {
+        _mainVcam = GameObject.Find("MainVcam").GetComponent<CinemachineVirtualCamera>();
+        _mainPerlin = _mainVcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
         CameraMove(1);
     }
 
@@ -28,9 +32,34 @@ public class CameraManager : MonoBehaviour
         }
     }
 
-
     public void CameraMove(int stageNum)
     {
         _mainVcam.Follow = _stageTrm[stageNum - 1];
+    }
+
+    public void ShakeCam(float intensity, float time)
+    {
+        if (_mainPerlin == null) return;
+        StopAllCoroutines();
+        StartCoroutine(ShakeCamCoroutine(intensity, time));
+    }
+
+    IEnumerator ShakeCamCoroutine(float intensity, float endtime)
+    {
+        _mainPerlin.m_AmplitudeGain = intensity;
+
+        float currentTime = 0f;
+        while(currentTime < endtime)
+        {
+            yield return new WaitForEndOfFrame();
+            if (_mainPerlin == null) break;
+
+            _mainPerlin.m_AmplitudeGain = Mathf.Lerp(intensity, 0, currentTime / endtime);
+            currentTime += Time.deltaTime;
+        }
+        if(_mainPerlin != null)
+        {
+            _mainPerlin.m_AmplitudeGain = 0;
+        }
     }
 }
