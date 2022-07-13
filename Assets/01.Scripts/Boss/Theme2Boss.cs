8e1ef3d9caa015011fe12177c5008a92b98d1c10
gusdnd01 <cestop42@naver.com>
2022-07-13 09:49:25 +0900

    220713-09시49분
    보스

--- Assets/01.Scripts/Boss/Theme2Boss.cs
+++ Assets/01.Scripts/Boss/Theme2Boss.cs
@@ -13,10 +13,11 @@ public class Theme2Boss : Boss
         _theme2BossAnim = GetComponent<Animator>();
     }
 
+    [ContextMenu("pattern1")]
     private void Pattern1_Enter()
     {
         Debug.Log("패턴 원");
-        _theme2BossAnim.SetTrigger("Theme2BossAttack1");
+        _theme2BossAnim.SetTrigger("BossAttack1");
         DartAttack();
     }
 
@@ -28,16 +29,17 @@ public class Theme2Boss : Boss
     IEnumerator DartCo()
     {
         GameObject warning = Instantiate(_warningPrefab_1);
-        warning.transform.localScale = new Vector3(4, 1);
-        warning.transform.position = new Vector3(transform.position.x + 1, 0, 0);
-        yield return new WaitForSeconds(0.3f);
+        warning.transform.localScale = new Vector3(4, 3);
+        warning.transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y - 1.5f, 0);
+        yield return new WaitForSeconds(1f);
         Destroy(warning);
     }
 
+    [ContextMenu("pattern2")]
     private void Pattern2_Enter()
     {
         Debug.Log("패턴 투");
-        _theme2BossAnim.SetTrigger("Theme2BossAttack2");
+        _theme2BossAnim.SetTrigger("BossAttack2");
         TurnAttack();
     }
 
@@ -49,15 +51,17 @@ public class Theme2Boss : Boss
     IEnumerator TurnCo()
     {
         GameObject warning = Instantiate(_warningPrefab_1);
-        warning.transform.position = transform.position;
-        yield return new WaitForSeconds(0.3f);
+        warning.transform.localScale = new Vector3(2, 4);
+        warning.transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y - 1.5f, 0);
+        yield return new WaitForSeconds(1f);
         Destroy(warning);
     }
 
+    [ContextMenu("pattern3")]
     private void Pattern3_Enter()
     {
         Debug.Log("패턴 쓰리");
-        _theme2BossAnim.SetTrigger("Theme2BossAttack3");
+        _theme2BossAnim.SetTrigger("BossAttack3");
         WhipAttack();
     }
 
@@ -69,8 +73,9 @@ public class Theme2Boss : Boss
     IEnumerator WhipCo()
     {
         GameObject warning = Instantiate(_warningPrefab_1);
-        warning.transform.position = transform.position;
-        yield return new WaitForSeconds(0.3f);
+        warning.transform.localScale = new Vector3(10, 2);
+        warning.transform.position = new Vector3(transform.position.x - 1f, transform.position.y - 2.5f, 0);
+        yield return new WaitForSeconds(1f);
         Destroy(warning);
     }
 }
