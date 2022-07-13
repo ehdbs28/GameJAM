b0a2a583c8dc47433b4481abe1ba2454eb347e65
ehdbs28 <doyun5373@naver.com>
2022-07-13 23:39:45 +0900

    수정

--- Assets/01.Scripts/Hyunwoong/Player/PlayerAttack.cs
+++ Assets/01.Scripts/Hyunwoong/Player/PlayerAttack.cs
@@ -281,7 +281,7 @@ public class PlayerAttack : MonoBehaviour
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         DashFx dashFx = PoolManager.Instance.Pop("DashFx") as DashFx;
         dashFx.transform.position = transform.localScale.x > 0 ? new Vector2(transform.position.x - 1, transform.position.y) : new Vector2(transform.position.x + 1, transform.position.y);
-        dashFx.transform.localScale = transform.localScale.x > 0 ? new Vector3(1, 1, 1) : new Vector3(-1, 1, 1);
+        dashFx.transform.localScale = transform.localScale.x > 0 ? new Vector3(2, 2, 2) : new Vector3(-2, 2, 2);
         Time.timeScale = 1;
     }
 
