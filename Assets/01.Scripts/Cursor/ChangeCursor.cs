7ad380691dbafbfb1b1484469c7e715533ecd60f
ehdbs28 <doyun5373@naver.com>
2022-08-25 19:42:18 +0900

    커서변경

--- Assets/01.Scripts/Cursor/ChangeCursor.cs
+++ Assets/01.Scripts/Cursor/ChangeCursor.cs
@@ -6,5 +6,8 @@ public class ChangeCursor : MonoBehaviour
 {
     [SerializeField] private Texture2D _cursorImage;
 
-
+    private void Awake()
+    {
+        Cursor.SetCursor(_cursorImage, new Vector3(_cursorImage.width / 2, _cursorImage.height / 2), CursorMode.Auto);
+    }
 }
